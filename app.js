e26b85fe0f3086801481eae56caeb10bf4da91d7
DinoPopsik <90846871+DinoPopsik@users.noreply.github.com>
2022-03-10 16:33:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-// const showBtn = document.querySelector('.navBtn');
-// const topNav = document.querySelector('.top-nav');
 
-// showBtn.addEventListener('click', function(){
-//     if(topNav.classList.contains('showNav')){
-//         topNav.classList.remove('showNav');
-//         showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
-//     }
-//     else {
-//         topNav.classList.add('showNav');
-//         showBtn.innerHTML = '<i class = "fas fa-times"></i>';
-//     }
-// });
 
 // var validateEmail = function(elementValue) {
 //     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
