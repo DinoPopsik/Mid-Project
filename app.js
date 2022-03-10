a3c2015e0c98f971fe03681e7c07ee28ddb88583
DinoPopsik <90846871+DinoPopsik@users.noreply.github.com>
2022-03-10 14:07:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const showBtn = document.querySelector('.navBtn');
-const topNav = document.querySelector('.top-nav');
+// const showBtn = document.querySelector('.navBtn');
+// const topNav = document.querySelector('.top-nav');
 
-showBtn.addEventListener('click', function(){
-    if(topNav.classList.contains('showNav')){
-        topNav.classList.remove('showNav');
-        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
-    }
-    else {
-        topNav.classList.add('showNav');
-        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
-    }
-});
+// showBtn.addEventListener('click', function(){
+//     if(topNav.classList.contains('showNav')){
+//         topNav.classList.remove('showNav');
+//         showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+//     }
+//     else {
+//         topNav.classList.add('showNav');
+//         showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+//     }
+// });
 
 function validateForm() {
    personName=document.forms['subs']['email'].value;
